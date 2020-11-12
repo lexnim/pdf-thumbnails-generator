7153490bff1ef6d37aed31e779c9934c186ed6b1
lexnim <sherpanimesh@gmail.com>
2020-11-12 05:57:09 +0000

    Build update

--- build/bundle.js
+++ build/bundle.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   pdf-thumbnails-generator v1.0.3
+ *   pdf-thumbnails-generator v1.0.4
  *   https://github.com/lexnim/pdf-thumbnails-generator
  *
  *   Copyright (c) Nimesh Sherpa (https://github.com/lexnim)
